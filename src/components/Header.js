0f67e512301a752c12c62c737360f7d27263f463
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-08-03 16:50:21 +0200

    Basic header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
+import { Box, makeStyles } from '@material-ui/core'
 import React from 'react'
+import header from '../assets/header.jpg'
+
 
 export default function Header() {
+    const style = styles();
+
     return (
-        <div>
-            
-        </div>
+        <Box className={style.root}>
+            <h1 className={style.logo}>Hyr din tomt</h1>
+            <img className={style.image} src={header} alt='Fence' />
+        </Box>
     )
-}
+};
+
+const styles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',  
+    },
+    image: {
+        width: '100%',
+        height: '90vh',
+        objectFit: 'cover',
+    },
+    logo: {
+        position: 'absolute',
+        zIndex: 10,
+    }
+}));
