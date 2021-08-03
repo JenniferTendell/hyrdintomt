5384198f10b6a78cb6da9e561861e0ffd5f19300
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-08-03 19:42:27 +0200

    Add images.

---
+++ src/components/Images.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box, Grid, makeStyles } from '@material-ui/core';
+
+/* IMAGES */
+import image1 from '../assets/image1.jpg';
+import image2 from '../assets/image2.jpg';
+import image3 from '../assets/image3.jpg';
+import image4 from '../assets/image4.jpg';
+
+export default function Images() {
+    const style = styles();
+
+    return (
+        <Box>
+            <Grid 
+                container 
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={3}
+            >
+                <Grid item xs={10} sm={5} md={6}>
+                    <img className={style.image} src={image1} alt='plot 1' />
+                </Grid>
+                <Grid item xs={10} sm={5} md={6}>
+                    <img className={style.image} src={image2} alt='plot 1' /> 
+                </Grid>
+                <Grid item xs={10} sm={5} md={6}>
+                    <img className={style.image} src={image3} alt='plot 1' />
+                </Grid>
+                <Grid item xs={10} sm={5} md={6}>
+                    <img className={style.image} src={image4} alt='plot 1' />
+                </Grid>
+            </Grid>
+        </Box>
+    )
+};
+
+const styles = makeStyles((theme) => ({
+    image: {
+        width: '100%',
+        objectFit: 'cover',
+        borderRadius: '4px',
+        boxShadow: '1px 2px 4px 1px rgb(0 0 0 / 20%)'
+    },   
+}));
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import Cards from './Cards'
-import InfoText from './InfoText'
+import React from 'react';
+import Cards from './Cards';
+import InfoText from './InfoText';
+import Images from './Images';
+import { makeStyles } from '@material-ui/core';
 
 export default function Main() {
+    const style = styles()
+
     return (
-        <main>
+        <main className={style.root}>
             <Cards />
             <InfoText />
-            
+            <Images />
         </main>
     )
-}
+};
+
+const styles = makeStyles((theme) => ({
+    root: {
+        maxWidth: '50rem',
+        margin: '0 auto',
+    }
+}))
